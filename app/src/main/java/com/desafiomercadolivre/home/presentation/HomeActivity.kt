f366d45965a498c81769e12e7703ed85077cb16b
Rodolfo Gusson <rodolfo.gusson@MacBook-Air-de-Rodolfo.local>
2025-03-20 16:09:14 -0300

    Use architecture helper functions in present activities

--- app/src/main/java/com/desafiomercadolivre/home/presentation/HomeActivity.kt
+++ app/src/main/java/com/desafiomercadolivre/home/presentation/HomeActivity.kt
@@ -1,21 +1,18 @@
 package com.desafiomercadolivre.home.presentation
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.desafiomercadolivre.R
+import com.desafiomercadolivre.architecture.extensions.useEdgeToEdge
+import com.desafiomercadolivre.architecture.extensions.viewBinding
+import com.desafiomercadolivre.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private val binding by viewBinding(ActivityHomeBinding::inflate)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(binding.root)
+        useEdgeToEdge()
     }
 }
--- app/src/main/java/com/desafiomercadolivre/search/presentation/SearchActivity.kt
+++ app/src/main/java/com/desafiomercadolivre/search/presentation/SearchActivity.kt
@@ -1,21 +1,18 @@
 package com.desafiomercadolivre.search.presentation
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.desafiomercadolivre.R
+import com.desafiomercadolivre.architecture.extensions.useEdgeToEdge
+import com.desafiomercadolivre.architecture.extensions.viewBinding
+import com.desafiomercadolivre.databinding.ActivitySearchBinding
 
 class SearchActivity : AppCompatActivity() {
+
+    private val binding by viewBinding(ActivitySearchBinding::inflate)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_search)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(binding.root)
+        useEdgeToEdge()
     }
 }
