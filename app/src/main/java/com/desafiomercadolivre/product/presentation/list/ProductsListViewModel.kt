9864d3b7868471476a07460e06e9d2a9c762777d
Rodolfo Gusson <rodolfo.gusson@MacBook-Air-de-Rodolfo.local>
2025-03-25 15:18:28 -0300

    Handle errors coming from api calls

--- app/src/main/java/com/desafiomercadolivre/product/presentation/list/ProductsListViewModel.kt
+++ app/src/main/java/com/desafiomercadolivre/product/presentation/list/ProductsListViewModel.kt
@@ -3,21 +3,34 @@ package com.desafiomercadolivre.product.presentation.list
 import androidx.lifecycle.viewModelScope
 import com.desafiomercadolivre.architecture.presentation.ViewModel
 import com.desafiomercadolivre.product.domain.usecase.SearchProductsUseCase
+import com.desafiomercadolivre.product.presentation.list.ProductsListViewModel.ProductsListError.NO_INTERNET
+import com.desafiomercadolivre.product.presentation.list.ProductsListViewModel.ProductsListError.UNKNOWN_ERROR
 import com.desafiomercadolivre.product.presentation.list.model.ProductsListAction
 import com.desafiomercadolivre.product.presentation.list.model.ProductsListAction.ShowSearchScreen
 import com.desafiomercadolivre.product.presentation.list.model.ProductsListState
 import kotlinx.coroutines.launch
+import java.io.IOException
 
 class ProductsListViewModel(
     val searchProductsUseCase: SearchProductsUseCase,
 ) : ViewModel<ProductsListState, ProductsListAction>(ProductsListState()) {
 
     fun searchProducts(query: String) = viewModelScope.launch {
-        val products = searchProductsUseCase(query)
-        changeState { it.copy(products = products, isLoading = false) }
+        runCatching {
+            val products = searchProductsUseCase(query)
+            changeState { it.copy(products = products, isLoading = false) }
+        }.onFailure(::handleError)
     }
 
     fun onSearchViewClicked() = sendAction { ShowSearchScreen }
+
+    private fun handleError(throwable: Throwable) {
+        val error = when (throwable) {
+            is IOException -> NO_INTERNET
+            else -> UNKNOWN_ERROR
+        }
+        changeState { it.copy(isLoading = false, products = null, error = error) }
+    }
 
     enum class ProductsListError {
         NO_INTERNET,
